3d0e175eda13f8bae02e9f466e99c036ae1256e2
ctokoli <matumasie@gmail.com>
2023-11-07 16:19:43 +0000

    Update greetingSlice.js

--- src/redux/greetingSlice.js
+++ src/redux/greetingSlice.js
@@ -1,40 +1,38 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-
-export const getGreeting = createAsyncThunk('get/message', 
-    async () => {
-        try {
-            const response  = await fetch("http://127.0.0.1:3000/api/greeting")
-            const result = await response.json()
-            console.log(result)
-            return result
-        } catch (error) {
-            console.error(error)
-        }
+export const getGreeting = createAsyncThunk('get/message',
+  async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:3000/api/greeting');
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-)
+  });
 
 const greetingSlice = createSlice({
-    name: 'greeting',
-    initialState: {
-        greeting:  '',
-        status: null,
-        error: null
-    },
-     extraReducers: (builder) => {
-        builder.addCase(getGreeting.pending, (state) => {
-            state.status = "Loading"
-        });
-        builder.addCase(getGreeting.fulfilled, (state, action) => {
-            state.status = "succeeded";
-            state.greeting = action.payload;
-        });
-        
-        builder.addCase(getGreeting.rejected, (state, action) => {
-            state.status = "failed";
-            state.error = action.error.message;
-        });
-     }
-})
+  name: 'greeting',
+  initialState: {
+    greeting: '',
+    status: null,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getGreeting.pending, (state) => {
+      state.status = 'Loading';
+    });
+    builder.addCase(getGreeting.fulfilled, (state, action) => {
+      state.status = 'succeeded';
+      state.greeting = action.payload;
+    });
+
+    builder.addCase(getGreeting.rejected, (state, action) => {
+      state.status = 'failed';
+      state.error = action.error.message;
+    });
+  },
+});
 
 export default greetingSlice.reducer;
