5abb74b90ce0d726481025da8759fc6007c45f53
ctokoli <matumasie@gmail.com>
2023-11-07 14:12:05 +0000

    Add greetings.js

---
+++ src/components/greetings.js
@@ -0,0 +1,22 @@
+import React, {useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getGreeting } from "../redux/greetingSlice";
+
+
+
+const Greeting = () => {
+    const greet = useSelector(state => state.greeting)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getGreeting());
+    }, [dispatch])
+    console.log(greet)
+    
+    return ( 
+        <div>
+            <h1>{greet.greeting.message}</h1>
+        </div>
+     );
+}
+ 
+export default Greeting;
